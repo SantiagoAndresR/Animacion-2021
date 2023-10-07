c0a9b5aec00b65a6396170307b9c0524d2f30a51
SantiagoAndresR <109627166+SantiagoAndresR@users.noreply.github.com>
2023-10-06 21:40:58 -0500

    yes

--- Assets/Script/MouseAim.cs
+++ Assets/Script/MouseAim.cs
@@ -2,28 +2,29 @@ using UnityEngine;
 
 public class MouseAim : MonoBehaviour
 {
-    public Transform armTarget; // Objeto de destino del brazo en el inspector
-    public float armRotationSpeed = 5.0f; // Velocidad de rotación del brazo
+    public Camera Cam;
+    Vector3 aim;
+    public float speed = 2f;
+    public GameObject target;
+    private bool isAiming = false;
 
     void Update()
     {
-        // Obtén la posición actual del mouse en pantalla
-        Vector3 mousePosition = Input.mousePosition;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+            isAiming = !isAiming;
 
-        // Convierte la posición del mouse a un punto en el mundo 3D
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 10f));
+          
+            target.SetActive(isAiming);
+        }
 
-        // Calcula la dirección desde el brazo hacia la posición del mouse
-        Vector3 direction = worldPosition - armTarget.position;
-
-        // Calcula la rotación necesaria para apuntar hacia el mouse
-        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
-
-        // Aplica la rotación al brazo (ajusta según tu jerarquía de huesos)
-        // Por ejemplo, si el brazo es un hijo del personaje y la mano es un hijo del brazo:
-        // armTarget.parent.rotation = rotation;
-
-        // Otra opción si usas Animation Rigging:
-        armTarget.parent.transform.rotation = Quaternion.Slerp(armTarget.parent.transform.rotation, rotation, armRotationSpeed * Time.deltaTime);
+   
+        if (isAiming)
+        {
+            aim = Input.mousePosition;
+            aim.z = speed;
+            transform.position = Cam.ScreenToWorldPoint(aim);
+        }
     }
 }
